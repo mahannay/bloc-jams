4cca65745c450805cc704cf7c21cb5c9ebf125b5
Megan <mahannay@gmail.com>
2018-03-12 12:40:14 -0400

    still working on checkpoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,27 +2,30 @@ var animatePoints = function() {
 
   var points = document.getElementsByClassName('point');
 
-  var revealFirstPoint = function() {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  var revealSecondPoint = function() {
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-  };
-
-  var revealThirdPoint = function() {
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-  };
-
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
+  for (i = 0; i > 3; i++) {
+    points[i].style.opacity = 1;
+    points[i].style.transform = "scaleX(1) translateY(0)";
+    points[i].style.msTransform = "scaleX(1) translateY(0)";
+    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
 
 };
+
+var animateHeader = function() {
+
+  var header = document.getElementsByClassName('hero-title');
+
+  var getBigger = function() {
+    header[0].style.transform = "scaleX(5) translateY(0)";
+  }
+  var getSmaller = function() {
+    header[0].style.transitiondelay = "2s";
+    header[0].style.transform = "scaleX(1) translateY(0)"
+  }
+
+  getBigger();
+  getSmaller();
+}
+
+animatePoints();
+animateHeader();
