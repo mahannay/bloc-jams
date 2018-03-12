4768eae809e8ed69f91f44052afdb322da5de91f
Megan <mahannay@gmail.com>
2018-03-12 16:30:57 -0400

    checkpoint 23 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
 
     for (i = 0; i < points.length; i++) {
       points[i].style.opacity = 1;
@@ -21,3 +21,19 @@ var animateHeader = function() {
   }
   getBigger();
 }
+
+window.onload = function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+         console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+     });
+ }
