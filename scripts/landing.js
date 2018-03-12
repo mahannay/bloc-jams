15da0711e44ca64b41ee70a65f0248de06eb34f2
Megan <mahannay@gmail.com>
2018-03-12 14:23:09 -0400

    assignment 22 finished

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,30 +2,22 @@ var animatePoints = function() {
 
   var points = document.getElementsByClassName('point');
 
-  for (i = 0; i > 3; i++) {
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
+    for (i = 0; i < points.length; i++) {
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
 
-};
+  };
+
 
 var animateHeader = function() {
 
   var header = document.getElementsByClassName('hero-title');
 
   var getBigger = function() {
-    header[0].style.transform = "scaleX(5) translateY(0)";
+    header[0].style.fontWeight = "600";
   }
-  var getSmaller = function() {
-    header[0].style.transitiondelay = "2s";
-    header[0].style.transform = "scaleX(1) translateY(0)"
-  }
-
   getBigger();
-  getSmaller();
 }
-
-animatePoints();
-animateHeader();
