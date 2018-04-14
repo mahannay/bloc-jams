a380e798cc37297ab67ec6ec72df737af4b57c38
Megan <mahannay@gmail.com>
2018-04-14 13:23:22 -0400

    checkpoint32 completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,46 @@
+//Example Album
+var albumPicasso = {
+  title: "The Colors",
+  artist: "Pablo Picasso",
+  label: "Cubism",
+  year: "1881",
+  albumArtUrl: "assets/images/album_covers/01.png",
+  songs: [
+    { title: "Blue", duration: "4:26" },
+    { title: "Green", duration: "3:14" },
+    { title: "Red", duration: "5:01" },
+    { title: "Pink", duration: "3:21"},
+    { title: "Magenta", duration: "2:15"}
+  ]
+};
+
+// Another Example Album
+var albumMarconi = {
+  title: "The Telephone",
+  artist: "Guglielmo Marconi",
+  label: "EM",
+  year: "1909",
+  albumArtUrl: "assets/images/album_covers/20.png",
+  songs: [
+    { title: "Hello, Operator?", duration: "1:01" },
+    { title: "Ring, ring, ring", duration: "5:01" },
+    { title: "Fits in your pocket", duration: "3:21" },
+    { title: "Can you hear me now?", duration: "3:14" },
+    { title: "Wrong phone number", duration: "2:15" },
+  ]
+};
+
+var albumDurham = {
+  title: "Bull City",
+  artist: "Durhamites",
+  label: "North Carolina",
+  year: "1869",
+  albumArtUrl: "assets/images/album_covers/06.png",
+  songs: [
+    {title: "Loud, loud train", duration: "4:34"},
+    {title: "Have you seen the new brewery?", duration: "3:21"},
+    {title: "Exam week coffee shops", duration: "2:09"},
+    {title: "Meet me at the food truck rodeo", duration: "3:13"},
+    {title: "Gentrification guilt", duration: "4:32"},
+  ]
+}
